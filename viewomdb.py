3f37b3edd097f2ce10d2a2fa1b65fef1eb090c52
avnibarad <51990481+avnibarad@users.noreply.github.com>
2019-08-14 12:13:28 +0530

    Update viewomdb.py

--- viewomdb.py
+++ viewomdb.py
@@ -2,7 +2,7 @@ import requests, json, sys
 from requests.exceptions import ConnectionError
 if len(sys.argv)>1:
         movie=" ".join(sys.argv[1:])
-        url='http://www.omdbapi.com/'
+        url="http://www.omdbapi.com/"
         movie_title=sys.argv[1:]
         try:
                 para=dict(t=movie_title,apikey='b1b6806f')
@@ -16,12 +16,11 @@ if len(sys.argv)>1:
                         else:
                                 print ('Rotten Tomatoes Ratings: '+ data[0]['Value'])
                 else:
-                        raise Exception('no Ratings Available for '+ movie)
+                       raise Exception('no Ratings Available for '+ movie)
         except ConnectionError as e:
                 print(e)
                 print('Either Url is not working or incorrect Url')
         except KeyError:
                 print(resp['Error'])
-
 else:
         raise Exception('Please pass argument as a movie name')
