e4cd096bea05de74a0fb3ec57c9bb8482255ad87
avnibarad <51990481+avnibarad@users.noreply.github.com>
2019-08-14 11:28:59 +0530

    Update viewomdb.py

--- viewomdb.py
+++ viewomdb.py
@@ -1,10 +1,27 @@
-argumentList=input("Enter Movie Name: ")
-print(argumentList)
-#pip install omdb
+import requests, json, sys
+from requests.exceptions import ConnectionError
+if len(sys.argv)>1:
+        movie=" ".join(sys.argv[1:])
+        url='http://www.omdbapi.com/'
+        movie_title=sys.argv[1:]
+        try:
+                para=dict(t=movie_title,apikey='b1b6806f')
+                resp = requests.get(url=url, params=para).json()
+                print('Movie name: '+movie)
+                ratings_list=resp['Ratings']
+                if len(ratings_list)>0:
+                        data=list(filter(lambda rating: rating['Source']=='Rotten Tomatoes',ratings_list))
+                        if len(data)==0:
+                                print ('Rotten Tomatoes Ratings not availabe')
+                        else:
+                                print ('Rotten Tomatoes Ratings: '+ data[0]['Value'])
+                else:
+                        raise Exception('no Ratings Available for '+ movie)
+        except ConnectionError as e:
+                print(e)
+                print('Either Url is not working or incorrect Url')
+        except KeyError:
+                print(resp['Error'])
 
-import omdb
-omdb.set_default('apikey', "b1b6806f")
-#client = omdb.OMDBClient(apikey="b1b6806f")
-response= omdb.search(argumentList,tomatoes=True)
-print(response)
-#print(client.get(title='True Grit',tomatoes=True))
+else:
+        raise Exception('Please pass argument as a movie name')
